b3ebf15a3be4364e07308a7e6fe91b0eb35e0e6a
MatthieuGuer <matthieu.guer@free.fr>
2026-04-16 16:26:19 +0100

    renaming

--- src/pymodaq_plugins_xy_stage_controller/app/custom_app_template.py
+++ src/pymodaq_plugins_xy_stage_controller/app/custom_app_template.py
@@ -5,7 +5,7 @@ from pymodaq_utils.config import Config
 from pymodaq_utils.logger import set_logger, get_module_name
 
 # todo: replace here *pymodaq_plugins_template* by your plugin package name
-from pymodaq_plugins_manipulator.utils import Config as PluginConfig
+from pymodaq_plugins_xy_stage_controller.utils import Config as PluginConfig
 
 logger = set_logger(get_module_name(__file__))
 
